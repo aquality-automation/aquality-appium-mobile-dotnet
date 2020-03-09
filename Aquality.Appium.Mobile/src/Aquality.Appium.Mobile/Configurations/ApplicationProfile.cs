d0598c6401b42408ae8953d0a00663f5eabc5c3f
Meleshko, Aleksey2 <a2.meleshko@a1qa.com>
2020-03-09 14:06:43 +0300

    Implement DriverSettings and ApplicationProfile

--- Aquality.Appium.Mobile/src/Aquality.Appium.Mobile/Configurations/ApplicationProfile.cs
+++ Aquality.Appium.Mobile/src/Aquality.Appium.Mobile/Configurations/ApplicationProfile.cs
@@ -1,16 +1,25 @@
 ﻿using Aquality.Appium.Mobile.Applications;
+using Aquality.Selenium.Core.Configurations;
+using Aquality.Selenium.Core.Utilities;
 using System;
 
 namespace Aquality.Appium.Mobile.Configurations
 {
     public class ApplicationProfile : IApplicationProfile
     {
-        public IDriverSettings DriverSettings => throw new NotImplementedException();
+        private readonly ISettingsFile settingsFile;
 
-        public bool IsRemote => throw new NotImplementedException();
+        public ApplicationProfile(ISettingsFile settingsFile)
+        {
+            this.settingsFile = settingsFile;
+        }
 
-        public PlatformName PlatformName => throw new NotImplementedException();
+        public IDriverSettings DriverSettings => new DriverSettings(settingsFile, PlatformName);
 
-        public Uri RemoteConnectionUrl => throw new NotImplementedException();
+        public bool IsRemote => settingsFile.GetValue<bool>(".isRemote");
+
+        public PlatformName PlatformName => settingsFile.GetValue<string>(".platformName").ToEnum<PlatformName>();
+
+        public Uri RemoteConnectionUrl => new Uri(settingsFile.GetValue<string>(".remoteConnectionUrl"));
     }
 }
