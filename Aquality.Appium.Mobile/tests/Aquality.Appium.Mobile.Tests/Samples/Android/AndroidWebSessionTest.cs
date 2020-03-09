344386bda26bcf6540dbdb77ae847b315a007e82
Meleshko, Aleksey2 <a2.meleshko@a1qa.com>
2020-03-09 15:15:50 +0300

    Add resource settings/localization files. Implement simple websession test

---
+++ Aquality.Appium.Mobile/tests/Aquality.Appium.Mobile.Tests/Samples/Android/AndroidWebSessionTest.cs
@@ -0,0 +1,35 @@
+﻿using Aquality.Appium.Mobile.Applications;
+using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using System;
+
+namespace Aquality.Appium.Mobile.Tests.Samples.Android
+{
+    public class AndroidWebSessionTest
+    {
+        private AndroidDriver<AppiumWebElement> driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Environment.SetEnvironmentVariable("profile", "androidwebsession");
+            AqualityServices.SetStartup(new MobileStartup());
+            driver = (AndroidDriver<AppiumWebElement>) AqualityServices.Application.Driver;
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            AqualityServices.Application.Quit();
+        }
+
+        [Test]
+        public void CreateWebSession()
+        {
+            driver.Navigate().GoToUrl("http://www.google.com");
+            var title = driver.Title;
+            Assert.AreEqual("Google", title);
+        }
+    }
+}
