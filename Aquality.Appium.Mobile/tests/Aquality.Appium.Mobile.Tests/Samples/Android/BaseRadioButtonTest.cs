a2cf1f05c74bf736a774f2f6607e5d5cf5a0bdb1
Meleshko, Aleksey2 <a2.meleshko@a1qa.com>
2020-03-25 18:06:49 +0300

    Add BaseRadioButtonTest

---
+++ Aquality.Appium.Mobile/tests/Aquality.Appium.Mobile.Tests/Samples/Android/BaseRadioButtonTest.cs
@@ -0,0 +1,24 @@
+﻿using Aquality.Appium.Mobile.Elements.Interfaces;
+using NUnit.Framework;
+
+namespace Aquality.Appium.Mobile.Tests.Samples.Android
+{
+    public abstract class BaseRadioButtonTest
+    {
+        protected abstract void OpenRadioButtonsScreen();
+
+        protected abstract IRadioButton GetRadioButton(int number);
+
+        public void TestCheckBox()
+        {
+            OpenRadioButtonsScreen();
+            IRadioButton button1 = GetRadioButton(1);
+            Assert.IsFalse(button1.IsChecked, "RadioButton should not be checked initially");
+            button1.Click();
+            Assert.IsTrue(button1.IsChecked, "RadioButton should be checked after click on it");
+            GetRadioButton(2).Click();
+            Assert.IsFalse(button1.IsChecked,
+                    $"RadioButton {button1.Name} should not be checked after click on another option");
+        }
+    }
+}
