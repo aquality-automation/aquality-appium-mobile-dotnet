8bb9bdcbab58b798da0a5f11773c748c11aaaf28
Meleshko, Aleksey2 <a2.meleshko@a1qa.com>
2020-03-25 17:49:03 +0300

    Add ITestCheckBox interface with default implementation to check c# 8.0 is used for tests

---
+++ Aquality.Appium.Mobile/tests/Aquality.Appium.Mobile.Tests/Samples/Android/ITestCheckBox.cs
@@ -0,0 +1,29 @@
+﻿using Aquality.Appium.Mobile.Elements.Interfaces;
+using NUnit.Framework;
+
+namespace Aquality.Appium.Mobile.Tests.Samples.Android
+{
+    public interface ITestCheckBox
+    {
+        void OpenCheckBoxesScreen();
+
+        ICheckBox GetCheckBox(int number);
+
+        public void TestCheckBox()
+        {
+            OpenCheckBoxesScreen();
+            var checkBox1 = GetCheckBox(1);
+            Assert.IsFalse(checkBox1.IsChecked, "Checkbox should not be checked initially");
+            checkBox1.Click();
+            Assert.IsTrue(checkBox1.IsChecked, "Checkbox should be checked after first click on it");
+            checkBox1.Uncheck();
+            Assert.IsFalse(checkBox1.IsChecked, "Checkbox should not be checked after uncheck");
+            checkBox1.Toggle();
+            Assert.IsTrue(checkBox1.IsChecked, "Checkbox should be checked after toggle from unchecked state");
+            checkBox1.Toggle();
+            Assert.IsFalse(checkBox1.IsChecked, "Checkbox should not be checked after toggle from checked state");
+            GetCheckBox(2).Check();
+            Assert.IsFalse(checkBox1.IsChecked, "Checkbox should not be checked after checking other checkbox");
+        }
+    }
+}
