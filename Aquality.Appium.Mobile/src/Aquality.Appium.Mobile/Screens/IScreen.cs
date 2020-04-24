0ab259879db1ced78b0096456472779b34370457
Pavel Anihimovsky <pavel.anihimovsky@gmail.com>
2020-04-24 16:29:27 +0300

    Add summary

--- Aquality.Appium.Mobile/src/Aquality.Appium.Mobile/Screens/IScreen.cs
+++ Aquality.Appium.Mobile/src/Aquality.Appium.Mobile/Screens/IScreen.cs
@@ -4,16 +4,34 @@ using System.Drawing;
 
 namespace Aquality.Appium.Mobile.Screens
 {
+    /// <summary>
+    /// Interface that describes application under the test screen.
+    /// </summary>
     public interface IScreen 
     {
+        /// <summary>
+        /// Unique screen locator.
+        /// </summary>
         By Locator { get; }
 
+        /// <summary>
+        /// Unique screen name.
+        /// </summary>
         string Name { get; }
 
+        /// <summary>
+        /// Gets Displayed screen's state by its locator.
+        /// </summary>
         bool IsDisplayed { get; }
 
+        /// <summary>
+        /// Size of the element described by screen locator.
+        /// </summary>
         Size Size { get; }
 
+        /// <summary>
+        /// Screen element State.
+        /// </summary>
         IElementStateProvider State { get; }
     }
 }
--- Aquality.Appium.Mobile/src/Aquality.Appium.Mobile/Screens/ScreenFactory.cs
+++ Aquality.Appium.Mobile/src/Aquality.Appium.Mobile/Screens/ScreenFactory.cs
@@ -1,8 +1,16 @@
 ﻿namespace Aquality.Appium.Mobile.Screens
 {
+    /// <summary>
+    /// Abstract screen factory.
+    /// </summary>
+    /// <typeparam name="TPlatformScreen">Desired platform screen <see cref="IOSScreen"/> or <see cref="AndroidScreen"/></typeparam>
     public abstract class ScreenFactory<TPlatformScreen> : IScreenFactory
         where TPlatformScreen : class
     {
+        /// <summary>
+        /// Returns an implementation of a particular app screen.
+        /// </summary>
+        /// <typeparam name="TAppScreen">Desired application screen.</typeparam>
         public abstract TAppScreen GetScreen<TAppScreen>() where TAppScreen : IScreen;
     }
 }
