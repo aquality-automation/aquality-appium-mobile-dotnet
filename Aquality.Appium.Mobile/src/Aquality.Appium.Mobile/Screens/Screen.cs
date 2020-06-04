61381b8011701258238b7b49de47072ff73b55df
Meleshko, Aleksey2 <a2.meleshko@a1qa.com>
2020-06-04 13:05:21 +0300

    Add documentation to screen methods. Make ElementFactory property static in to make it possible to be used during the field initialization

--- Aquality.Appium.Mobile/src/Aquality.Appium.Mobile/Screens/Screen.cs
+++ Aquality.Appium.Mobile/src/Aquality.Appium.Mobile/Screens/Screen.cs
@@ -8,10 +8,20 @@ using IElementFactory = Aquality.Appium.Mobile.Elements.Interfaces.IElementFacto
 
 namespace Aquality.Appium.Mobile.Screens
 {
+    /// <summary>
+    /// Defines base class for any mobile screen.
+    /// Use <see cref="ScreenFactory.ScreenTypeAttribute"/> on your screen class to identify platform.
+    /// <seealso cref="ScreenFactory.ScreenFactory"/>
+    /// </summary>
     public abstract class Screen : IScreen
     {
         private readonly ILabel screenLabel;
 
+        /// <summary>
+        /// Constructor with parameters.
+        /// </summary>
+        /// <param name="locator">Unique locator of the screen.</param>
+        /// <param name="name">Name of the screen.</param>
         protected Screen(By locator, string name)
         {
             Locator = locator;
@@ -19,16 +29,35 @@ namespace Aquality.Appium.Mobile.Screens
             screenLabel = ElementFactory.GetLabel(locator, name);
         }
 
+        /// <summary>
+        /// Gets Screen element defined by its locator and name.
+        /// Could be used to find child elements relative to screen element.
+        /// </summary>
+        protected IElement ScreenElement => screenLabel;
+        
+        /// <summary>
+        /// Element factory <see cref="IElementFactory"/>
+        /// </summary>
+        protected static IElementFactory ElementFactory => AqualityServices.ElementFactory;
+        
+        /// <summary>
+        /// Locator of the screen.
+        /// </summary>
         public By Locator { get; }
 
+        /// <summary>
+        /// Name of the screen.
+        /// </summary>
         public string Name { get; }
 
+        /// <summary>
+        /// Gets size of form element defined by its locator.
+        /// </summary>
         public Size Size => screenLabel.GetElement().Size;
 
+        /// <summary>
+        /// Provides ability to get screen's state (whether it is displayed, exists or not) and respective waiting functions.
+        /// </summary>
         public IElementStateProvider State => screenLabel.State;
-
-        protected IElementFactory ElementFactory => AqualityServices.ElementFactory;
-
-        protected IElement ScreenElement => screenLabel;
     }
 }
